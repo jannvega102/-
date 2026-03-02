72677cdc4cf8a971b1fec74f26fb65afe855bd41
Jann Niño Armar Trufil Vega <141836123+jannvega102@users.noreply.github.com>
2026-03-02 21:31:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Use the repo name as the base path
+  // GitHub Pages URL will be: username.github.io/-/
   base: '/-/', 
   build: {
     outDir: 'dist',
