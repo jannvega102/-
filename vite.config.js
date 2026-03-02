29d7f8ac13d176c7b82da6f64aad6c9094bbc5bb
Jann Niño Armar Trufil Vega <141836123+jannvega102@users.noreply.github.com>
2026-03-02 21:36:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // GitHub Pages URL will be: username.github.io/-/
+  /** * This is the "Magic Fix" for GitHub Pages 
+   * It tells Vite that the app is hosted in the /-/ subfolder
+   */
   base: '/-/', 
   build: {
     outDir: 'dist',
